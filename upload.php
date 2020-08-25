1e929b3a72d0f2f63bf1755a20b02a8ab6f5788a
Pyry Pynnönen <pyry.pynnonen@itvelhot.local>
2020-08-25 13:34:04 +0300

    Vaihdoin upload-tekstisivun tyyliä.

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 <html>
 <body>
   <link rel="stylesheet" href="style.css">
-
+<div class="center">
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -23,35 +23,37 @@ if(isset($_POST["submit"])) {
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  echo "<br /><br /> Sorry, file already exists.";
   $uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000000) {
-  echo "Sorry, your file is too large.";
+  echo "<br /><br /> Sorry, your file is too large.";
   $uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  echo "<br /><br /> Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Your file was not uploaded.";
+  echo "<br /><br /> Your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    echo "<br /><br /> Sorry, there was an error uploading your file.";
   }
 }
 ?>
+<br>
+<br>
 <button onclick="goBack()">Takaisin etusivulle</button>
 
 <script>
@@ -59,5 +61,6 @@ function goBack() {
   window.history.back();
 }
 </script>
+</div>
 </body>
 </html>

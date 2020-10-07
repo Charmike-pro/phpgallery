a520b78ec09d717b0633c059e533e97e4213b8a6
Char Mike <storagewars999@gmail.com>
2020-10-07 09:03:33 +0300

    Lisäsimme piilotus-attribuutin.

--- display.php
+++ display.php
@@ -7,18 +7,22 @@
     <title>Palautesivu</title>
 </head>
 <body>
+
+
 <?php 
 $xml = simplexml_load_file('data.xml');
 ?>
 
-<?php foreach ($xml->picture as $pic):
-?>
-<div>
-<h2><?php echo $pic->author; ?></h2>
-<img src="uploads/<?php echo $pic->file;?>" alt="kuva" />
-<p><?php echo $pic->date; ?></p>
-</div>
+<?php foreach ($xml->picture as $pic):?>
+    <div>
+    <?php if ($pic->attributes()['piilota'] == "false"): ?>
+        <h2><?php echo $pic->author; ?></h2>
+        <img src="uploads/<?php echo $pic->file;?>" alt="kuva" />
+        <p><?php echo $pic->date; ?></p>
+        <?php endif; ?>
+        </div>
 <?php endforeach; ?>
+
 
 </body>
 </html>
